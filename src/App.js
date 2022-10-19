2127ae8462f7b01a780e92d221d9561e3e2547fd
Ramanujam Gond <ramanujamgond@gmail.com>
2022-10-19 19:46:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("Ramanujam");
+  const [flag, setFlag] = useState(false);
+
+  function changeName() {
+    return setFlag(!flag);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello, {flag ? name : "Priyanka"}</div>
+      <br />
+      <button onClick={changeName}>Click Me!</button>
     </div>
   );
 }
